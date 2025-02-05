e345e37a0736051f2d6c2ca0311c398da0929c6e
Harry Holmes <harryholmesse@gmail.com>
2025-02-05 16:35:33 +0100

    Added Interface and repository with basic structure for a base repository

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<bool> AddAsync(TEntity entity);
+    Task<IEnumerable<TEntity>?> GetAllAsync();
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression);
+    Task<bool> UpdateAsync(TEntity entity);
+    Task<bool> DeleteAsync(TEntity entity);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+}
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public class BaseRepository<TEntity>(DbContext context) : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly DbContext _context = context;
+    protected readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
+
+    public Task<bool> AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TEntity>?> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+}
